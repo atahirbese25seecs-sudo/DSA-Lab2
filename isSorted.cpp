fb8a4fc7a461c6f46f6b5bb93e41479f9856f4eb
Amman <atahir.bese25seecs@seecs.edu.pk>
2026-09-19 01:45:27 +0500

    Implement isSorted function

--- isSorted.cpp
+++ isSorted.cpp
@@ -5,11 +5,15 @@ using namespace std;
 // Function declaration
 bool isSorted(const int* arr, const int size);
 
-// Temporary implementation for TDD
 bool isSorted(const int* arr, const int size) {
-    return false;
-}
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
 
+    return true;
+}
 // Test 1: Sorted ascending array
 void testSortedAscending() {
     int arr[] = {1, 2, 3, 4, 5};
